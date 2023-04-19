3ea98e0e96f93ad793c8bdb9ebaf2cf1772033fe
Franklyn Roberto da Silva <franklyn1998@live.com>
2023-04-19 18:04:13 -0300

    First Steps in Java with Spring Boot 3

---
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/github/hadesfranklyn/project/Startup.java
@@ -0,0 +1,13 @@
+package com.github.hadesfranklyn.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
