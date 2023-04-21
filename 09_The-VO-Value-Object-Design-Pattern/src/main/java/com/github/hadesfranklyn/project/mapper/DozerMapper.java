5d629419e25a6b103dfb3bf5d2db9f8f4bc0e4c7
Franklyn Roberto da Silva <franklyn1998@live.com>
2023-04-21 15:11:41 -0300

    Differences between Spring Boot 3 Milestone and Spring Boot 3.0.1

--- 09_The-VO-Value-Object-Design-Pattern/src/main/java/com/github/hadesfranklyn/project/mapper/DozerMapper.java
+++ 09_The-VO-Value-Object-Design-Pattern/src/main/java/com/github/hadesfranklyn/project/mapper/DozerMapper.java
@@ -3,12 +3,16 @@ package com.github.hadesfranklyn.project.mapper;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
+import org.modelmapper.ModelMapper;
+
+//import com.github.dozermapper.core.DozerBeanMapperBuilder;
+//import com.github.dozermapper.core.Mapper;
 
 public class DozerMapper {
 
-	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+//	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	private static ModelMapper mapper = new ModelMapper();
+	
 
 	public static <O, D> D parseObject(O origin, Class<D> destination) {
 		return mapper.map(origin, destination);
